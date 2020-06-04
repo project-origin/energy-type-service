258c7f5e3355d441e6646b07275ae7824bbb39d1
Martin Schmidt <martin@scoding.dk>
2020-06-04 15:06:08 +0000

    Added to get codes from parquet file.

---
+++ src/file_energycodes.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from hashlib import sha256
+
+from .exception import EnergyCodeNotFoundException
+from .settings import ENERGYCODE_FILE
+
+def hash_gsrn(data):
+
+    x = str(data).encode()
+
+    for i in range(1000):
+        x = sha256(x).digest()
+
+    return x.hex()
+
+
+def get_tech_fuel_code(gsrn):
+    """
+    :rtype: (str, str)
+    :return: Tuple of (technologyCode, fuelCode)
+    """
+
+    hashed_gsrn = hash_gsrn(gsrn)
+    df = pd.read_parquet(ENERGYCODE_FILE)
+
+    if hashed_gsrn in df.index:
+
+        row = df.loc[hashed_gsrn]
+
+        return row.tech_code, row.fuel_code
+
+    else:
+        raise EnergyCodeNotFoundException()
